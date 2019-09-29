03a9ddc2a7c2ce56c2c557611f318f437780ee1f
jingyuanz <barrel.titor.daru@gmail.com>
2019-09-29 15:23:54 +0800

    fix re2

--- re2layer.py
+++ re2layer.py
@@ -7,7 +7,7 @@ import numpy as np
 class Encoder(Module):
     def __init__(self, dim):
         super(Encoder, self).__init__()
-        self.bilstm = LSTM(dim, dim, bidirectional=True, dropout=0.2, num_layers=1)
+        self.bilstm = LSTM(dim*2, dim, bidirectional=True, dropout=0.2, num_layers=1)
     
     def forward(self, x):
         t_h, (_, _) = self.bilstm(x)
@@ -31,17 +31,15 @@ class RE2(Module):
         
         o_t2b = T.zeros_like(emb_a, dtype=T.float32)
         o_t1b = T.zeros_like(emb_a, dtype=T.float32)
-        
         assert self.num_block > 0
         for i in range(self.num_block):
-            inp_a = T.cat([emb_a, o_t1a + o_t2a])
-            inp_b = T.cat([emb_b, o_t1b + o_t2b])
+            inp_a = T.cat([emb_a, o_t1a + o_t2a], dim=-1)
+            inp_b = T.cat([emb_b, o_t1b + o_t2b], dim=-1)
             o_a, o_b = self.blocks[i](inp_a, inp_b)
             o_t2a = o_t1a
             o_t1a = o_a
             o_t2b = o_t1b
             o_t1b = o_b
-        
         y = self.predictor(o_a, o_b)
         return y
 
@@ -60,23 +58,22 @@ class RE2Block(Module):
         fused_a = self.fuser(encoded_a, aligned_a)
         aligned_b = self.aligner(encoded_b, encoded_a)
         fused_b = self.fuser(encoded_b, aligned_b)
-
         return fused_a, fused_b
         
         
 class Fuser(Module):
     def __init__(self, dim):
         super(Fuser, self).__init__()
-        self.G1 = Linear(dim * 2, dim)
-        self.G2 = Linear(dim * 2, dim)
-        self.G3 = Linear(dim * 2, dim)
+        self.G1 = Linear(dim * 8, dim)
+        self.G2 = Linear(dim * 8, dim)
+        self.G3 = Linear(dim * 8, dim)
         self.G = Linear(dim * 3, dim)
         
     def _fuse(self, z, z_new):
-        z1 = self.G1(T.cat([z, z_new]))
-        z2 = self.G2(T.cat([z, z-z_new]))
-        z3 = self.G3(T.cat([z, z*z_new]))
-        z_o = self.G(T.cat([z1,z2,z3]))
+        z1 = self.G1(T.cat([z, z_new],dim=-1))
+        z2 = self.G2(T.cat([z, z-z_new],dim=-1))
+        z3 = self.G3(T.cat([z, z*z_new],dim=-1))
+        z_o = self.G(T.cat([z1,z2,z3],dim=-1))
         return z_o
     
     def forward(self, z, z_new):
@@ -87,15 +84,14 @@ class Fuser(Module):
 class Aligner(Module):
     def __init__(self, dim):
         super(Aligner, self).__init__()
-        self.ff = Linear(dim, dim)
+        self.ff = Linear(dim*4, dim*4)
         
     def forward(self, ix, iother):
         ex = self.ff(ix)
         eother = self.ff(iother)
         align_x = ex.bmm(eother.transpose(1,2))
-        align_x = F.softmax(align_x)
+        align_x = F.softmax(align_x, dim=-1)
         aligned = align_x.bmm(iother)
-        print(aligned.size())
         return aligned
     
     
@@ -105,9 +101,9 @@ class Prediction(Module):
         self.H = Linear(dim*4, 1)
         
     def forward(self, a, b):
-        a = T.max(a)
-        b = T.max(b)
-        y = self.H(T.cat([a,b,T.abs(a-b),a*b]))
+        a = T.max(a, dim=1)[0]
+        b = T.max(b, dim=1)[0]
+        y = self.H(T.cat([a,b,T.abs(a-b),a*b],dim=-1))
         y = F.sigmoid(y)
         return y
 
